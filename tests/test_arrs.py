19d5facbe553c4f8c765e7466818438685b433bf
Kusto7 <kirill.kustov21@gmail.com>
2023-04-01 02:48:02 +0300

    Покрыл 100% кода тестами

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -8,4 +8,12 @@ def test_get():
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], 0) == [1, 2, 3, 4]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 0) == []
+    assert arrs.my_slice([], 4) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -4) == [4, 5, 6, 7]
+    assert arrs.my_slice([-1, -2, -3, -4, -5, -6, -7], -4) == [-4, -5, -6, -7]
+    assert arrs.my_slice([-1, -2, -3, -4, -5, -6, -7], 4) == [-5, -6, -7]
+    assert arrs.my_slice([-1, -2, -3, -4, -5, -6, -7], 0) == [-1, -2, -3, -4, -5, -6, -7]
+    assert arrs.my_slice([], -4) == []
